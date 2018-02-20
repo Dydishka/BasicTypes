b82dcf19ae5f7eb1ce25c6db91b4bd3db9e68adc
Alexander <alexvolotkevich@gmail.com>
2018-02-20 13:01:49 +0300

    ex2

---
+++ src/basictypes/Seven.java
@@ -0,0 +1,15 @@
+package basictypes;
+
+import java.util.*;
+
+public class Seven {
+    public static void main(String[] args) {
+        Random z = new Random();
+        int x = z.nextInt(70);
+        if (x % 10 == 7) {
+            System.out.println( x + " является цифрой 7");
+        } else {
+            System.out.println( x + " не является цифрой 7");
+        }
+    }
+}
