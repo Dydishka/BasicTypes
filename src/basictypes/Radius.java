474c341abc23c8f4630708dbdde03d65607d1469
Alexander Volotkevich <36504545+Dydishka@users.noreply.github.com>
2018-02-27 09:03:50 +0300

    Update Radius.java

--- src/basictypes/Radius.java
+++ src/basictypes/Radius.java
@@ -9,10 +9,7 @@ public class Radius {
         int b = random.nextInt(50) + 1;
         int r = random.nextInt(40) + 1;
         System.out.println(a + " " + b + " " + r);
-        int z;
-        if (a >= b) z = a;
-        else z = b;
-        if (2 * r >= z) {
+        if (4 * r * r >= a * a + b * b) {
             System.out.println("Прямоугольник полность закрыт кругом с радиусом " + r);
         } else {
             System.out.println("Прямоугольник не закрыт кругом с радиусом " + r);
